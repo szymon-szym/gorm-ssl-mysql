78f730ea0044181eff1885aae6cc224743b524b3
szymon-szym <szymon.mich@gmail.com>
2024-06-23 16:23:59 +0200

    Initial commit

---
+++ test/cert_test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CertTest from '../lib/cert_test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cert_test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CertTest.CertTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
